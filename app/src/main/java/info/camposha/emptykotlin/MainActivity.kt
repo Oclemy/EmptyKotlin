5385f2994d23723f5b94c1fc8a40b0fd12b18fa2
Clement Ochieng <oclemmi@gmail.com>
2021-06-11 03:47:07 +0300

    Update MainActivity.kt

--- app/src/main/java/info/camposha/emptykotlin/MainActivity.kt
+++ app/src/main/java/info/camposha/emptykotlin/MainActivity.kt
@@ -2,10 +2,14 @@ package info.camposha.emptykotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import info.camposha.emptykotlin.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
